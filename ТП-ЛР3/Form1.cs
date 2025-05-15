0f722beda2ebb47cac02cba134b92841cfcf75a9
erago <erago@PC>
2025-05-15 14:19:26 +0300

    Изменено название namespace, добавлена ветвь Dev, метод InitializationДобавьте файлы проекта.

---
+++ ТП-ЛР3/Form1.cs
@@ -0,0 +1,16 @@
+namespace TP_LR3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Функция для настройки формы при загрузке
+        public void Initialization()
+        {
+
+        }
+    }
+}
